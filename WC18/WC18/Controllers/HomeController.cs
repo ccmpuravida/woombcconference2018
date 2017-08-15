84937146a84dbf48e119b6a968f8505658ecc569
codercrhub <gbermude@outlook.com>
2017-08-14 22:15:55 -0600

    Paso a ASP.NET MVC

---
+++ WC18/WC18/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WC18.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //return View("Index.en");
+            return View();
+        }
+
+        public ActionResult Program()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Speakers()
+        {
+            return View();
+        }
+
+        public ActionResult ChangeLanguage()
+        {
+            return View();
+        }
+    }
+}
